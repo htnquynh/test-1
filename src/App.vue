93f894e50ed1fd7d3e9fabd86227e1b6b83ed915
quynh_hoang78 <qhoang@hrcloud.com>
2024-10-02 16:09:26 +0700

    fix link target

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 
 <template>
   <main>
-    <a href="https://quynhtest1.netlify.app">Open other site with map</a>
+    <a href="https://quynhtest1.netlify.app" target="_blank" rel="nofollow">Open other site with map</a>
   </main>
 </template>
 
